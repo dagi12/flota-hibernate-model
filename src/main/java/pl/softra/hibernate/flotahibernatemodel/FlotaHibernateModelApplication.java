6021a07ce99ceb1e143ce6072bec7828bfb08aeb
Eryk Mariankowski <dagi12@o2.pl>
2018-04-24 22:20:45 +0200

    initial commit

---
+++ src/main/java/pl/softra/hibernate/flotahibernatemodel/FlotaHibernateModelApplication.java
@@ -0,0 +1,12 @@
+package pl.softra.hibernate.flotahibernatemodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlotaHibernateModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlotaHibernateModelApplication.class, args);
+	}
+}
